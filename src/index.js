d3ec7551d439e4a62fcf1aee45c3633064edf1c2
DanGRT <d.geraghty06@gmail.com>
2018-09-13 10:20:59 +0100

    adds tweet to timeline

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const formElement = document.querySelector("form")
+const textBoxElement = document.querySelector(".user-text")
+const timelineElement = document.querySelector(".timeline")
 formElement.addEventListener("submit", event => {
   event.preventDefault()
   console.log("default prevented")
+  const latestTweet = document.querySelector(".latest-tweet")
+  const userInput = textBoxElement.value
+  const newTweet = document.createElement("div")
+  newTweet.setAttribute("class", "latest-tweet")
+  newTweet.textContent = userInput
+  timelineElement.insertBefore(newTweet, latestTweet)
+  textBoxElement.textContent = ""
+
+
+
+
+
 })
